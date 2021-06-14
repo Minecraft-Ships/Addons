f7562f316388f16e506ce2de82e9d1fc998389fc
Mose <nekronmoseley@gmail.com>
2021-06-14 10:31:04 +0100

    Fixed vessel flag

--- VaultAddon/src/main/java/org/ships/addon/vault/CreateListener.java
+++ VaultAddon/src/main/java/org/ships/addon/vault/CreateListener.java
@@ -1,13 +1,17 @@
 package org.ships.addon.vault;
 
+import org.core.adventureText.AText;
+import org.core.adventureText.format.NamedTextColours;
 import org.core.entity.Entity;
 import org.core.entity.living.human.player.LivePlayer;
 import org.core.event.EventListener;
+import org.core.event.HEvent;
 import org.core.event.events.entity.EntityEvent;
 import org.ships.event.vessel.create.VesselCreateEvent;
 
 public class CreateListener implements EventListener {
 
+    @HEvent
     public void onVesselCreate(final VesselCreateEvent.Pre event) {
         double price = event.getVessel().getType().getFlagValue(CreatePriceFlag.class).orElse(0.0);
         if (price == 0.0) {
@@ -16,16 +20,17 @@ public class CreateListener implements EventListener {
         if (!(event instanceof EntityEvent)) {
             return;
         }
-        final Entity<?> entity = ((EntityEvent<?>)event).getEntity();
+        final Entity<?> entity = ((EntityEvent<?>) event).getEntity();
         if (!(entity instanceof LivePlayer)) {
             return;
         }
-        final LivePlayer player = (LivePlayer)entity;
+        final LivePlayer player = (LivePlayer) entity;
         if (player.hasPermission("vaultaddon.ignore.create")) {
             return;
         }
         if (!player.removeBalance(price)) {
-            player.sendMessagePlain(event.getVessel().getType().getDisplayName() + " costs " + price);
+            AText message = AText.ofPlain(event.getVessel().getType().getDisplayName() + " costs " + price).withColour(NamedTextColours.RED);
+            player.sendMessage(message);
             event.setCancelled(true);
         }
     }
--- VaultAddon/src/main/java/org/ships/addon/vault/CreatePriceFlag.java
+++ VaultAddon/src/main/java/org/ships/addon/vault/CreatePriceFlag.java
@@ -6,8 +6,7 @@ import org.ships.vessel.common.flag.VesselFlag;
 
 import java.util.Optional;
 
-public class CreatePriceFlag implements VesselFlag.Serializable<Double>
-{
+public class CreatePriceFlag implements VesselFlag.Serializable<Double> {
     private double price;
 
     public CreatePriceFlag() {
@@ -18,54 +17,62 @@ public class CreatePriceFlag implements VesselFlag.Serializable<Double>
         this.price = price;
     }
 
+    @Override
     public String serialize() {
         return this.price + "";
     }
 
+    @Override
     public VesselFlag.Serializable<Double> deserialize(final String idWithValue) {
         try {
             this.price = Double.parseDouble(idWithValue);
+        } catch (NumberFormatException ex) {
         }
-        catch (NumberFormatException ex) {}
         return this;
     }
 
+    @Override
     public boolean isDeserializable(final String idWithValue) {
         try {
             Double.parseDouble(idWithValue);
             return true;
-        }
-        catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             return false;
         }
     }
 
+    @Override
     public Optional<Double> getValue() {
         return Optional.of(this.price);
     }
 
+    @Override
     public void setValue(final Double value) {
         this.price = value;
     }
 
+    @Override
     public StringParser<Double> getParser() {
         return Parser.STRING_TO_DOUBLE;
     }
 
+    @Override
     public Builder toBuilder() {
         return new Builder();
     }
 
+    @Override
     public String getId() {
         return VaultAddon.getInstance().getCoreAddon().getPluginId() + ":price";
     }
 
+    @Override
     public String getName() {
         return "Price";
     }
 
-    public class Builder extends VesselFlag.Builder<Double, CreatePriceFlag>
-    {
+    public static class Builder extends VesselFlag.Builder<Double, CreatePriceFlag> {
+        @Override
         protected CreatePriceFlag buildEmpty() {
             return new CreatePriceFlag();
         }
