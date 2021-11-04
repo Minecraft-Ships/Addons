9f566ce477b9d6cddfd57305e357d78cb63e0d7b
ThatPerkel <thatperkel@gmail.com>
2021-11-04 09:11:22 +0100

    Code improvements

--- VaultAddon/src/main/java/org/ships/addon/vault/VaultAddon.java
+++ VaultAddon/src/main/java/org/ships/addon/vault/VaultAddon.java
@@ -1,7 +1,7 @@
 package org.ships.addon.vault;
 
 import org.core.platform.plugin.*;
-import org.core.platform.plugin.details.PluginVersion;
+import org.core.platform.plugin.details.CorePluginVersion;
 import org.core.TranslateCore;
 import org.core.command.CommandRegister;
 import org.core.config.ConfigurationFormat;
@@ -13,7 +13,7 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.util.Optional;
 
-public class VaultAddon implements Plugin {
+public class VaultAddon implements CorePlugin {
     static BukkitVaultAddon plugin;
 
     public String getPluginName() {
@@ -24,8 +24,8 @@ public class VaultAddon implements Plugin {
         return "vaultaddon";
     }
 
-    public PluginVersion getPluginVersion() {
-        return new VaultAddonVersion(0, 2);
+    public CorePluginVersion getPluginVersion() {
+        return new CorePluginVersion(0, 3, 0);
     }
 
     public void registerCommands(final CommandRegister register) {
@@ -58,5 +58,20 @@ public class VaultAddon implements Plugin {
 
     public BukkitVaultAddon getPlatformLauncher() {
         return this.getLauncher();
+    }
+
+    @Override
+    public void onConstruct(Object pluginLauncher) {
+
+    }
+
+    @Override
+    public void onRegisterCommands(CommandRegister register) {
+
+    }
+
+    @Override
+    public String getLicence() {
+        return "All Rights Reserved";
     }
 }
